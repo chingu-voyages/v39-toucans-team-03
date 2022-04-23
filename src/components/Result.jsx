77449bead71fffbea9ab53578abb652ee4c6f9df
hyerimYeom <hyerim.yeom@kakao.com>
2022-04-24 03:53:25 +0900

    #16 Made a view with grid layout
    - 3part all have their grid-parents
    - Made result.json sample
    - Set grid style and deleted effect for typing-demo one
    [options]
    - Made other grid layout in Figma (would mention in the next meeting)

--- src/components/Result.jsx
+++ src/components/Result.jsx
@@ -2,61 +2,60 @@ import React from "react";
 import '../assets/css/style_result.css';
 import resultPic from "../assets/img/sample_result.jpg";
 
-// results: Array(2)
-// 0: {lion: 0}
-// 1: {cow: 0}
-
 function Result(props) {
-    
-    // const results = props.results;
-
-    // const final_result = () =>{
-    //     results.map((res)=>{
-    //         if(res.lion > res.cow){
-    //             return res
-    //         }
-    //     })
-    // }
-
     return ( 
-    <div className="center">
-        <div className="center">
-            <span className="center typing-demo" >Result</span>
-        </div>
-        <div className="result_picture">
-            <img src={resultPic} className="result_picture_elements" alt="The flower that matches your personality" />
-            <div>
-                
+    <div>
+        <div className="grid-parent">
+            <span className="title center">Result</span>
+            <img src={resultPic} className="picture result_picture_elements" alt="" />
+            <div className="diagram">
+                circle
             </div>
         </div>
-        <div className ="checkList">
-            <span className="center">Check List</span>
-            <ul class="myclass">
-                <li><label><input type="checkbox"/><span>some text</span></label></li>
-                <li><label><input type="checkbox"/><span>some text</span></label></li>
-                <li><label><input type="checkbox"/><span>some text</span></label></li>
-            </ul>
-           
+        <div className ="grid-parent checkList">
+            <span className="title center">Check List</span>
+            <div class="items">
+                <div><label><input type="checkbox"/><span>Results sentences are in here esults sentences are in hereesults sentences are in hereesults sentences are in hereesults sentences are in hereesults sentences are in hereesults sentences are in hereesults sentences are in here</span></label></div>
+                <div><label><input type="checkbox"/><span>Results sentences are in here</span></label></div>
+                <div><label><input type="checkbox"/><span>Results sentences are in here</span></label></div>
+                <div><label><input type="checkbox"/><span>Results sentences are in here</span></label></div>
+                <div><label><input type="checkbox"/><span>Results sentences are in here</span></label></div>
+            </div>
         </div>
-        <div>
-            <span className="center">share</span>
-            <div>
+        <div className="grid-parent">
+            <span className="title center">Share</span>
+            <div className="items">
                 <span>weChat</span>
                 <span>Messenger</span>
                 <span>KaKao talk</span>
             </div>
         </div>
     </div>
-    
-    // <div>
-    //     {props.results.map((e, idx) => (
-    //             <div key={idx}>
-    //                 {Object.keys(e)}:{Object.values(e)}
-    //             </div>
-    //         ))}
-    // </div> 
-
     );
 }
 
 export default Result;
+
+
+// results: Array(2)
+// 0: {lion: 0}
+// 1: {cow: 0}
+
+ 
+// const results = props.results;
+
+// const final_result = () =>{
+//     results.map((res)=>{
+//         if(res.lion > res.cow){
+//             return res
+//         }
+//     })
+// }
+
+// <div>
+//     {props.results.map((e, idx) => (
+//             <div key={idx}>
+//                 {Object.keys(e)}:{Object.values(e)}
+//             </div>
+//         ))}
+// </div> 
