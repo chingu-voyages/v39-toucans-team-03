556b916f6520666d3d6cb7b211bde297e012cd41
hyerimYeom <hyerim.yeom@kakao.com>
2022-05-04 00:28:40 +0900

    # Fix the space
    - Changed factor 6 to 7 as cover the whole height of website
    - It occurred # of questions is decreased 5 to 4

--- src/App.js
+++ src/App.js
@@ -13,6 +13,7 @@ import lain1 from "./assets/img/049.jpg";
 import lain2 from "./assets/img/050.jpg";
 import lain3 from "./assets/img/063.jpg";
 import lain4 from "./assets/img/095.jpg";
+import questions_json from "./assets/json/questions.json"
 
 const lains = [lain0, lain1, lain2, lain3, lain4];
 
@@ -20,28 +21,7 @@ function App() {
     const ref = useRef();
     
     const [results, set_results] = useState( {introvert: 0 ,  extrovert: 0, count : 0} );
-    const [questions] = useState([
-        {
-            question: "On a honey-like weekend?",
-            one: ["After all, I should take a rest while watching TV at home on weekends.", "introvert"],
-            two: ["The house is so boring! I will go anywhere!", "extrovert"],
-        },
-        {
-            question: "What is my first reaction when I am angry?",
-            one: ["I’m filled with anger and I'm crying.", "introvert"],
-            two: ["Speak rationally about the reason for my anger.", "extrovert"],
-        },
-        {
-            question: "Which one is closer to me? I think I know where my things are, but when I look for them,",
-            one: ["I am well organized and find where my things are.", "introvert"],
-            two: ["I can't find them...", "extrovert"],
-        },
-        {
-            question: "Your friend asks you to drag you in front of her/his friend of opposite sex and have a conversation.",
-            one: ["I’m shy, so I just smile quietly and be awkward.", "introvert"],
-            two: ["Laugh and have fun talking together.", "extrovert"],
-        }
-    ]);
+    const [questions] = useState(questions_json);
     
     let isFinished = results.count === Object.keys(questions).length ? true : false;
 
@@ -70,7 +50,7 @@ function App() {
                         backgroundImage: `url(${bg})`,
                         backgroundSize: "cover",
                     }}
-                    factor={6}
+                    factor={7}
                 ></ParallaxLayer>
                 <ParallaxLayer
                     offset={0}
