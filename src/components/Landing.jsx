1d5c3bd9cf6578d02328307da00816069a83bd04
hyerimYeom <hyerim.yeom@kakao.com>
2022-05-14 18:30:52 +0900

    :speech_balloon: Change introduction in main page

--- src/components/Landing.jsx
+++ src/components/Landing.jsx
@@ -5,12 +5,12 @@ function Landing(props) {
                 <div className="landing">
                     <h1>
                         <div className="line1"></div>
-                        Lost <span className="fontStyle">&</span> Found
+                        <span className="fontStyle">Your Personality</span> 
+                        <h2>- I OR E?</h2>
                         <div className="line2"></div>
                     </h1>
-
-                    <h2>What kind of flower am I ?</h2>
                     <p>Find out by answering this short array of questions !</p>
+                    <p>Check out the result and quote for you.</p>
                     <button>Start the Test</button>
                 </div>
             </header>
